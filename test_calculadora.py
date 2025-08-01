ac1baa6c9296a6051638616ed08a7a8670816400
João Lucas <joao.lgomes1166@gmail.com>
2025-08-01 10:56:06 -0300

    Unittest #1

---
+++ test_calculadora.py
@@ -0,0 +1,18 @@
+import unittest
+from calculadora import soma
+
+class TestCalculadora(unittest.TestCase):
+    def test_soma(self):
+        self.assertEqual(soma(1, 2), 3)
+
+    def test_soma_varias_entradas(self):
+      x_y_saidas = (( 1, 2, 3),
+                    (0, 0, 0),
+                    (-1, 1, 0),
+                    (2.5, 2.5, 5.0),
+    )
+
+      for x, y, saida in x_y_saidas:
+          self.assertEqual(soma(x, y), saida)
+
+unittest.main(verbosity=2)
