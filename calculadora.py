3829d3b1d3b8de69cfe9f50599bb5f0bd117f4dc
João Lucas <joao.lgomes1166@gmail.com>
2025-08-01 10:15:15 -0300

    Asserções(Assertions)

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,4 @@
 def soma(a, b):
+    assert isinstance(a, (int, float)), "O primeiro argumento deve ser um número"
+    assert isinstance(b, (int, float)), "O segundo argumento deve ser um número"
     return a + b
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from calculadora import soma
 
 resultado = soma(5, 3)
 print(f"A soma é: {resultado}")
+print(soma('15', 5))  # Isso deve gerar um erro de asserção
