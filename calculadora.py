1ec219c3099556b99c21c0b6e7df42afb3efd106
João Lucas <joao.lgomes1166@gmail.com>
2025-08-01 10:33:07 -0300

    Doctests

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,14 @@
 def soma(a, b):
+    """
+    Função que soma dois números.
+    >>> soma(5, 3)
+    8
+    """
     assert isinstance(a, (int, float)), "O primeiro argumento deve ser um número"
     assert isinstance(b, (int, float)), "O segundo argumento deve ser um número"
     return a + b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
+    print("Todos os testes passaram!")
